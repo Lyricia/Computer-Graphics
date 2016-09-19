1dcd901b533a1180ab1350aa938e8540696a0472
Kang subin <lyricia01@gmail.com>
2016-09-19 14:58:30 +0900

    update

--- Computer Graphics/Exam 3/Exam 3/main.cpp
+++ Computer Graphics/Exam 3/Exam 3/main.cpp
@@ -16,6 +16,8 @@ GLvoid MouseMove(int, int);
 GLvoid MouseEvent(int, int, int, int);
 GLvoid init(GLvoid);
 
+GLvoid Prot_Draw(GLvoid);
+
 GLvoid DrawPolygon(GLvoid);
 
 
@@ -38,12 +40,24 @@ int poly_count = 0;
 void main(int, char *)
 {
 	init();
+
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(W_Width, W_Height);
 	glutCreateWindow("Test");
+
 	RegesterCallBack();
 	glutMainLoop();
+}
+
+GLvoid init(GLvoid)
+{
+	space_x = 600 / 3;
+	space_y = 600 / 3;
+
+	for (int i = 0; i < 50; i++)
+		pt[i] = { 0,0,2 };
+
 }
 
 GLvoid RegesterCallBack()
@@ -77,7 +91,74 @@ GLvoid drawScene(GLvoid)
 	}
 	glEnd();
 
+	Prot_Draw();
 
+	glFlush();
+}
+
+GLvoid Reshape(int w, int h)
+{
+	glViewport(0, 0, w, h);
+	glOrtho(0.0, 600.0, 0.0, 600.0, -1.0, 1.0);
+}
+
+
+GLvoid Keydown(unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 'q':
+		exit(0);
+		break;
+
+	case 'r':
+		init();
+		break;
+
+	case 'e':
+		if (pt_index > 0)
+			pt[pt_index - 1].end = 1;
+		break;
+	}
+
+	glutPostRedisplay();
+}
+
+GLvoid MouseMove(int x, int y)
+{
+	
+}
+
+GLvoid MouseEvent(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
+	{
+		pt[pt_index] = { x,600 - y,0 };
+		pt_index++;
+		poly_count++;
+	}
+
+}
+
+GLvoid DrawPolygon(GLvoid)
+{
+	for (int i = 0; pt[i].end != 2; i++)
+	{
+		
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glLineWidth(5);
+		glBegin(GL_POLYGON);
+
+		//for (int i = pt_index - poly_count; i < poly_count; i++)
+		//	glVertex2i(pt[i].x, pt[i].y);
+
+		glEnd();
+
+	}
+}
+
+GLvoid Prot_Draw(GLvoid)
+{
 	glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 	glLineWidth(5);
 	glBegin(GL_LINES);
@@ -145,75 +226,4 @@ GLvoid drawScene(GLvoid)
 	}
 	glEnd();
 
-	glFlush();
-}
-
-GLvoid Reshape(int w, int h)
-{
-	glViewport(0, 0, w, h);
-	glOrtho(0.0, 600.0, 0.0, 600.0, -1.0, 1.0);
-}
-
-GLvoid init(GLvoid)
-{
-	space_x = 600 / 3;
-	space_y = 600 / 3;
-
-	for (int i = 0; i < 50; i++)
-		pt[i] = { 0,0,2 };
-
-}
-
-GLvoid Keydown(unsigned char key, int x, int y)
-{
-	switch (key)
-	{
-	case 'q':
-		exit(0);
-		break;
-
-	case 'r':
-		init();
-		break;
-
-	case 'e':
-		if (pt_index > 0)
-			pt[pt_index - 1].end = 1;
-		break;
-	}
-
-	glutPostRedisplay();
-}
-
-GLvoid MouseMove(int x, int y)
-{
-	
-}
-
-GLvoid MouseEvent(int button, int state, int x, int y)
-{
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
-	{
-		pt[pt_index] = { x,600 - y,0 };
-		pt_index++;
-		poly_count++;
-	}
-
-}
-
-GLvoid DrawPolygon(GLvoid)
-{
-	for (int i = 0; pt[i].end != 2; i++)
-	{
-		
-		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-		glLineWidth(5);
-		glBegin(GL_POLYGON);
-
-		//for (int i = pt_index - poly_count; i < poly_count; i++)
-		//	glVertex2i(pt[i].x, pt[i].y);
-
-		glEnd();
-
-	}
 }
