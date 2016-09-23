34ba6fb9d1cfcc343fd44f3b0733a0a9a03deb19
Kang subin <lyricia01@gmail.com>
2016-09-23 19:30:58 +0900

    update

--- Computer Graphics/Exam 9/Exam 9/main.cpp
+++ Computer Graphics/Exam 9/Exam 9/main.cpp
@@ -12,14 +12,7 @@ struct Point
 {
 	float x;
 	float y;
-	float prev_x;
-	float prev_y;
-	int dir_x;
-	int dir_y;
-	int live;				// 1 right 2 left 3 up 4 down
-	float rad;
-	float rad_speed;
-	float rad_limit;
+	float radius;
 };
 
 GLvoid RegesterCallBack();
@@ -33,28 +26,16 @@ GLvoid MouseMove(int, int);
 GLvoid MouseEvent(int, int, int, int);
 GLvoid init(GLvoid);
 
-GLvoid Menu(int);
 GLvoid DrawPolygon(GLvoid);
-GLvoid CollideChk(GLvoid);
 
 GLvoid Circle(int, float, float);
 
-
-Point RectMove(Point);
-
-int space_x;
-int space_y;
 
 Point pt[20];
 int pt_index = 0;
 int poly_count = 0;
 
 int val_timer = 50;
-int convert = -1;
-
-int moveval = 0;	// 1 move 0 stop
-
-int rad = 0;
 
 void main(int, char *)
 {
@@ -74,25 +55,13 @@ void main(int, char *)
 
 GLvoid init(GLvoid)
 {
-	space_x = W_Width / 3;
-	space_y = W_Height / 3;
-
 	for (int i = 0; i < 50; i++)
-		pt[i] = { 
-		0,0,
-		0,0,
-		0,0,
-		2,0.0, 0 };
+		pt[i] = { 0,};
 
 }
 
 GLvoid RegesterCallBack()
 {
-	glutCreateMenu(Menu);
-	glutAddMenuEntry("Rect", 1);
-	glutAddMenuEntry("Circle", 2);
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
-
 	glutMouseFunc(MouseEvent);
 	glutMotionFunc(MouseMove);
 	glutKeyboardFunc(Keydown);
@@ -105,23 +74,6 @@ GLvoid drawScene(GLvoid)
 {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
-
-
-	//glColor4f(1.0f, 1.0f, 0.0f, 1.0f);
-	//glLineWidth(5);
-	//glBegin(GL_LINES);
-	//{
-	//	glVertex2i(space_x, 0);
-	//	glVertex2i(space_x, W_Height);
-	//	glVertex2i(space_x * 2, 0);
-	//	glVertex2i(space_x * 2, W_Height);
-	//
-	//	glVertex2i(0, space_y);
-	//	glVertex2i(W_Width, space_y);
-	//	glVertex2i(0, space_y * 2);
-	//	glVertex2i(W_Width, space_y * 2);
-	//}
-	//glEnd();
 
 	DrawPolygon();
 
@@ -174,10 +126,8 @@ GLvoid MouseEvent(int button, int state, int x, int y)
 	{
 		if (pt_index > 9)
 			pt_index = 0;
-		pt[pt_index] = { (float)x,(float)W_Height - y,(float)x,(float)W_Height - y };
-		pt[pt_index].rad = 0;
-		pt[pt_index].rad_speed = 1;
-		pt[pt_index].rad_limit = 0;
+		pt[pt_index] = { (float)x,(float)W_Height - y};
+		pt[pt_index].radius = 0;
 
 		pt_index++;
 		if (poly_count<10)
@@ -190,32 +140,16 @@ GLvoid DrawPolygon(GLvoid)
 {
 	for (int index = 0; index < poly_count; index++)
 	{
-		if (convert == 1)
-		{ 
-			glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-			glBegin(GL_POLYGON);
-			glVertex2i(pt[index].x - RectSize_X, pt[index].y - RectSize_Y);
-			glVertex2i(pt[index].x + RectSize_X, pt[index].y - RectSize_Y);
-			glVertex2i(pt[index].x + RectSize_X, pt[index].y + RectSize_Y);
-			glVertex2i(pt[index].x - RectSize_X, pt[index].y + RectSize_Y);
-			glEnd();
-		}
-		else if (convert == -1)
+		glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+		glLineWidth(2);
+		glPointSize(3);
+		glBegin(GL_POINTS);
+		for (float angle = 0; angle != 360; angle +=5)
 		{
-			glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
-			glPointSize(3);
-			glBegin(GL_POINTS);
-			//for (rad = 0; rad < 360 * 4; rad += 10)
-			//{
-			// 
-			//}
-			while (pt[index].rad_speed < pt[index].rad_limit) 
-			{
-				Circle(index, pt[index].rad, pt[index].rad_speed);
-				pt[index].rad_speed += 5;
-			}
-			glEnd();
+			Circle(index, pt[index].radius, angle);
 		}
+
+		glEnd();
 	}
 }
 
@@ -223,69 +157,23 @@ GLvoid Timer(int val)
 {
 	for (int index = 0; index < poly_count; index++)
 	{
-		pt[index].rad_limit += 5;
-		if (pt[index].rad_limit > 360 * 2)
-			break;
-		pt[index].rad+=0.5;
+			pt[index].radius += 5;
 	}
 
 	glutPostRedisplay();
 	glutTimerFunc(val_timer, Timer, 1); 
 }
 
-
-GLvoid CollideChk()
-{
-	for (int i = 0; i < poly_count; i++)
-	{
-		if (pt[i].x < 0)
-		{
-			pt[i].dir_x = 10;
-			pt[i].dir_y = 0;
-		}
-
-		else if (pt[i].y < 0)
-		{
-			pt[i].dir_y = 10;
-			pt[i].dir_x = 0;
-		}
-
-		else if (pt[i].x > W_Width)
-		{
-			pt[i].dir_x = -10;
-			pt[i].dir_y = 0;
-		}
-		else if (pt[i].y > W_Height)
-		{
-			pt[i].dir_y = -10;
-			pt[i].dir_x = 0;
-		}
-	}
-}
-
-GLvoid Menu(int button)
-{
-	switch (button)
-	{
-	case 1:
-		convert = 1;
-		break;
-	case 2:
-		convert = -1;
-		break;
-	}
-	glutPostRedisplay();
-}
-
 GLvoid Circle(int index, float radius, float angle)
 {
 	angle = angle * (3.141592 / 180);
-	float x=0;
-	float y=0;
-	
-	x = (cos(angle)*radius + pt[index].x);
-	y = (sin(angle)*radius + pt[index].y);
 
-	glVertex2f(x, y);
+	float circlex=0;
+	float circley=0;
+	
+	circlex = (cos(angle)*radius + pt[index].x);
+	circley = (sin(angle)*radius + pt[index].y);
+
+	glVertex2f((cos(angle)*radius + pt[index].x), (sin(angle)*radius + pt[index].y));
 }
 
