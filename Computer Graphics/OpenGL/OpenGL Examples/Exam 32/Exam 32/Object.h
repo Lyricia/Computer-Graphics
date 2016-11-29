bfa81dea5003b9e020ea832f0df98c5210898a83
Kang subin <lyricia01@gmail.com>
2016-11-29 19:52:54 +0900

    update

---
+++ Computer Graphics/OpenGL/OpenGL Examples/Exam 32/Exam 32/Object.h
@@ -0,0 +1,33 @@
+#pragma once
+#include "ObjProp.h"
+
+class CObject : public ObjProp
+{
+public:
+	CObject();
+	~CObject();
+
+	void Move(float Speed, bool isMovex, bool isMovey, bool isMovez);
+
+private:
+	float m_Matrix[16];
+	int m_RectTopAngle;
+	float m_SphereScaleSize;
+	float m_TorusMovePos;
+	float m_ConeScaleSize;
+	float m_DoorPos;
+	float ydir;
+public:
+	void RectTreeRender();
+	void rotateRect(int angle);
+	void SphereTreeRender();
+	void scaleSphere(float scale);
+	void TorusBuildingRender();
+	void moveTorus(int speed);
+	void ConeBuildingRender();
+	void scaleCone(float scale);
+	void DoorBuildingRender();
+	void moveDoor(float speed);
+	void PyramidRender();
+};
+
